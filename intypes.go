a966f120639525e7d3fa63a7e4f1af61a077da3f
Denis Cheremisov <denis.cheremisov@gmail.com>
2023-05-10 09:47:40 +0300

    fix: type aliases

--- intypes.go
+++ intypes.go
@@ -2,20 +2,20 @@ package intypes
 
 type (
 	// VI16 to represent int16 with varint encoding.
-	VI16 int16
+	VI16 = int16
 
 	// VI32 to represent int32 with varint encoding.
-	VI32 int32
+	VI32 = int32
 
 	// VI64 to represent int64 with varint encoding.
-	VI64 int64
+	VI64 = int64
 
 	// VU16 to represent uint16 with varint encoding.
-	VU16 int16
+	VU16 = int16
 
 	// VU32 to represent uint32 with varint encoding.
-	VU32 int32
+	VU32 = int32
 
 	// VU64 to represent uint64 with varint encoding.
-	VU64 int64
+	VU64 = int64
 )
