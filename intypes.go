d0cd290da34c7405f50b9c7033cbcab1b552d7ef
Denis Cheremisov <denis.cheremisov@gmail.com>
2023-05-15 15:08:46 +0300

    fix: omg

--- intypes.go
+++ intypes.go
@@ -11,11 +11,11 @@ type (
 	VI64 = int64
 
 	// VU16 to represent uint16 with varint encoding.
-	VU16 = int16
+	VU16 = uint16
 
 	// VU32 to represent uint32 with varint encoding.
-	VU32 = int32
+	VU32 = uint32
 
 	// VU64 to represent uint64 with varint encoding.
-	VU64 = int64
+	VU64 = uint64
 )
